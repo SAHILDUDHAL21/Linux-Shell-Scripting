124930b8f7a147aaba9634d6ee440426cf8bdaff
Sahil Dudhal <129080375+SAHILDUDHAL21@users.noreply.github.com>
2023-09-27 19:48:36 +0530

    Add files via upload

---
+++ update-&-upgrade.sh
@@ -0,0 +1,15 @@
+sudo apt-get update
+
+sudo apt-get upgrade -y
+
+sudo apt-get autoremove -y
+
+sudo apt-get clean -y
+
+echo "System updates and upgrade completed"
+echo "............................."
+
+echo "Rebooting the System ........"
+
+reboot
+
